255fb46af605c154802e68df74dce88cc14b7a75
joydip-paul <joydippaul72@gmail.com>
2025-08-06 03:19:38 +0600

    task 1

---
+++ function.py
@@ -0,0 +1,17 @@
+# Why use functions?
+# Avoid repeating code
+# Organize logic
+# Make debugging easier
+
+#GREETINGS
+def greeting(name):
+    print("KAMAN ASA", name)
+greeting("JOYDIP")
+greeting("Subhashree")
+
+#SUM
+def lets_add(first_number, second_number):
+    return first_number + second_number
+
+summation = lets_add(11,9)
+print(summation)
---
+++ task1.py
@@ -0,0 +1,26 @@
+# 📝 Task: Build a Simple Calculator Function
+# Requirements:
+# Write four functions:
+# add(a, b) — returns the sum of a and b.
+# subtract(a, b) — returns a minus b.
+# multiply(a, b) — returns the product of a and b.
+# divide(a, b) — returns a divided by b. (Hint: handle division by zero!)
+# Then, call each function with some example numbers and print the results.
+
+
+# CALCULATOR FUNCTIONS
+def add(a,b):
+    return a+b
+def subtract(a,b):
+    return a-b
+def multiply (a,b):
+    return a*b
+def divide(a,b):
+    if b == 0:
+        raise ZeroDivisionError("Cannot divide by zero")
+    return a/b
+
+print("Add:", add(10, 20))
+print("Subtract:", subtract(50, 30))
+print("Multiply:", multiply(5, 6))
+print("Divide:", divide(50, 5))
